af668c582fcd34430409995f1d8508ad578be32d
HEOJIHYEOK <vndj1212@gmail.com>
2025-01-16 18:41:55 +0900

    UnitaskTesting

---
+++ PROJECT-TST/Assets/PROJECT TST/Scripts/UnitaskTest.cs
@@ -0,0 +1,42 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace TST
+{
+    public class UnitaskTest : MonoBehaviour
+    {
+        private CancellationTokenSource cancel = new CancellationTokenSource();
+        // Start is called before the first frame update
+        private async void Start()
+        {
+            TestUni(cancel.Token).Forget();
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                cancel.Cancel();
+            }
+        }
+
+        private async UniTask TestUni(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                Debug.Log("TestUni");
+                await UniTask.Delay(100, true, cancellationToken: cancellationToken);
+            }
+        }
+    }
+}
